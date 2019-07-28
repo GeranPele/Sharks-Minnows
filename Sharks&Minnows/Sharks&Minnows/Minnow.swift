d49ac21cb389bb4e7a722e252d979ea6e1507c71
Geran <geranpele@gmail.com>
2019-07-28 14:59:40 -0700

    Started 'master' project and begun creating appropriate classes

---
+++ Sharks&Minnows/Sharks&Minnows/Minnow.swift
@@ -0,0 +1,14 @@
+//
+//  Minnow.swift
+//  Sharks&Minnows
+//
+//  Created by Geran Pele on 7/28/19.
+//  Copyright © 2019 Geran Pele. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class Minnow: SCNNode {
+
+}
